afd2d28f7a1784380adab6d854287718672f1f7d
beatricher <isolivefausris@gmail.com>
2026-04-26 18:04:29 +0300

    feat(Ui_MainWindow) : Добавлен класс для основного окна MainWindow windows(Ui_MainWindow) : Добавлен класс главного окна gui : Реализован класс отвечающий за работу основного окна

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from classes import *
 from windows import *
+from PySide6.QtWidgets import QApplication, QMainWindow 
+app = QApplication()
+window = QMainWindow()
+ui = MainWindow()
+ui.setupUi(window)
+window.show()
+app.exec()
